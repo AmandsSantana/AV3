c4587d5c43850a7aade4763a6e7a8d67f54a1780
kamillita <102391081+kamillita@users.noreply.github.com>
2022-06-12 17:34:05 -0300

    Update AV3.c

--- AV3.c
+++ AV3.c
@@ -6,7 +6,28 @@
 
 int main(void) {
   
-  //Kamilla, só colocar aqui  a Main  
+  char *mensagem;
+  char *mensagemSaida;
+  char original[1];
+  char novo[1];
+  
+  mensagem = malloc(256 * sizeof(int));
+
+  printf("Digite uma palavra: ");
+  scanf("%s", mensagem);
+  printf("Informa a letra que deseja trocar: ");
+  scanf(" %c", original);
+  printf("Informe a letra que irá substituir: ");
+  scanf(" %c", novo);
+  
+  int quantidadeTrocas = TrocaCaracter(&mensagem, original[0], novo[0], &mensagemSaida, strlen(mensagem));
+
+  printf("Nova palavra é: ");
+  printf("%s\n", mensagemSaida);
+  printf("Palavra original: ");
+  printf("%s\n", mensagem);
+  printf("Quantidade de trocas: ");
+  printf("%d\n", quantidadeTrocas);
   
   return 0;
 }
